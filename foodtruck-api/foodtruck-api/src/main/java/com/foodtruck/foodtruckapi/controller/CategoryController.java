b48e3571b32a5636368d991de114f9a6f1d7b9e1
Noemi Roldan <noemi.roldan@peopleshores.com>
2026-03-18 16:52:35 -0400

    Model Category created

---
+++ foodtruck-api/foodtruck-api/src/main/java/com/foodtruck/foodtruckapi/controller/CategoryController.java
@@ -0,0 +1,22 @@
+package com.foodtruck.foodtruckapi.controller;
+
+import com.foodtruck.foodtruckapi.model.Category;
+import com.foodtruck.foodtruckapi.repository.CategoryRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/categories")
+@RequiredArgsConstructor
+public class CategoryController {
+    private final CategoryRepository categoryRepository;
+
+    @GetMapping
+    public List<Category> getAllCategories(){
+        return categoryRepository.findAll();
+    }
+}
---
+++ foodtruck-api/foodtruck-api/src/main/java/com/foodtruck/foodtruckapi/model/Category.java
@@ -0,0 +1,26 @@
+package com.foodtruck.foodtruckapi.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+
+import java.time.LocalDateTime;
+
+@NoArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE)
+@AllArgsConstructor
+@Data
+@Entity
+@Table(name = "Categories")
+public class Category {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    //@Column(name = "CategoryID")
+    Integer categoryId;
+
+    //@Column(name = "Name", nullable = false, length = 100)
+    String name;
+
+    //@Column(name = "CreatedAt")
+    LocalDateTime createdAt;
+}
